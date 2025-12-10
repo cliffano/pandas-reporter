333b4fccd0c586d36c9de40bcd71981b3c7136e7
Cliffano Subagio <cliffano@gmail.com>
2025-12-10 13:14:55 +1100

    Add text test scenario with nested data.

--- tests/formatters/test_text.py
+++ tests/formatters/test_text.py
@@ -6,14 +6,23 @@ from pandasreporter.formatters.text import format_report
 
 class TestTextFormatter(unittest.TestCase):
 
-    def test_format_report(self):
+    def test_format_report_with_empty_data(self):
+        data = {}
+        data_frame = pd.DataFrame(data)
+        result = format_report(data_frame, {})
+        expected = (
+            ""
+        )
+        self.assertEqual(result, expected)
+
+    def test_format_report_with_simple_data(self):
         data = {
             "Name": ["Barkley", "Pippen", "Robinson"],
             "DOB": ["19630220", "19650925", "19650806"],
             "City": ["Philadelphia", "Chicago", "San Antonio"],
         }
         data_frame = pd.DataFrame(data)
-        result = format_report(data_frame, {"out_format": "text"})
+        result = format_report(data_frame, {})
         expected = (
             "┌──────────┬──────────┬──────────────┐\n"
             "│ Name     │      DOB │ City         │\n"
@@ -24,5 +33,28 @@ class TestTextFormatter(unittest.TestCase):
             "├──────────┼──────────┼──────────────┤\n"
             "│ Robinson │ 19650806 │ San Antonio  │\n"
             "└──────────┴──────────┴──────────────┘"
+        )
+        self.assertEqual(result, expected)
+
+    def test_format_report_with_nested_data(self):
+        data = {
+            "Players": {
+                "Barkley": {"DOB": "19630220", "City": "Philadelphia"},
+                "Pippen": {"DOB": "19650925", "City": "Chicago"},
+                "Robinson": {"DOB": "19650806", "City": "San Antonio"},
+            }
+        }
+        data_frame = pd.DataFrame(data)
+        result = format_report(data_frame, {})
+        expected = (
+            "┌─────────────────────────────────────────────┐\n"
+            "│ Players                                     │\n"
+            "├─────────────────────────────────────────────┤\n"
+            "│ {'DOB': '19630220', 'City': 'Philadelphia'} │\n"
+            "├─────────────────────────────────────────────┤\n"
+            "│ {'DOB': '19650925', 'City': 'Chicago'}      │\n"
+            "├─────────────────────────────────────────────┤\n"
+            "│ {'DOB': '19650806', 'City': 'San Antonio'}  │\n"
+            "└─────────────────────────────────────────────┘"
         )
         self.assertEqual(result, expected)
